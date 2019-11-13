7ae427477974d223a41e75dd991b030cf4e879ac
Kelsey Low <kklowlow@gmail.com>
2019-11-13 03:42:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ document.body.removeChild(main);
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
 newHeader.innerHTML = 'YOUR-NAME is the champion';
-document.body.appendChid(newHeader)
+document.body.appendChid(newHeader);
